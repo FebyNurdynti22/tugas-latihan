046c46a8f8453861f40bedb813f294ec04a8c690
FebyNurdynti22 <73349859+FebyNurdynti22@users.noreply.github.com>
2021-01-09 14:26:55 +0700

    Add files via upload

---
+++ PersegiPanjang.java
@@ -0,0 +1,26 @@
+public class PersegiPanjang
+{
+	private int panjang;
+	private int lebar;
+	
+	public void setPanjang(int panjang)
+	{
+		this.panjang = panjang; //memberikan nilai variabel panjang dengan nilai yang ada di dalam parameter
+	}
+	
+	public int getPanjang()
+	{
+		return panjang; //memberikan nilai kembalian
+	}
+	
+	public void setLebar(int lebar)
+	{
+		this.lebar = lebar; //memberikan nilai variabel lebar dengan nilai yang ada di dalam parameter
+	}
+	
+	public int getLebar()
+	{
+		return lebar; //memberikan nilai kembalian
+	}
+	
+}
